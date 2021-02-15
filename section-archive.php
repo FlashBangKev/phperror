415bf84fa21e76c0c337afccbd8bdee64d70fd11
FlashBangKev <56278539+FlashBangKev@users.noreply.github.com>
2021-02-15 12:12:41 +0000

    Add files via upload
    Line 7
    syntax error, unexpected '<'

---
+++ section-archive.php
@@ -0,0 +1,31 @@
+<?php
+get_header();
+ 
+if ( have_posts() ) : 
+    while ( have_posts() ) : the_post();
+ 
+                        <div class="blog-card">
+                            <img src="./img/test1.jpg" alt="" class="blog-thumbnail">
+                           <div class="cardtext-wrapper">
+                                <h3 class="blogtitle">
+                                    <?php the_title();?>
+                                </h3>
+                                <p class="blog-preview">
+                                    <?php the_excerpt();?>
+                                    <a href="<?php the_permalink();?>">
+                                </p>
+
+                           </div>
+                        </div>
+                       
+
+
+    endwhile;
+else :
+    _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+endif;
+
+get_footer();
+
+?>
+
